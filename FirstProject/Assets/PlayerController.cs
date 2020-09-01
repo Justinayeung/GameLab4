c737e1d4dad1af22fbe3b9a81e168afb13780a38
Justinayeung <yeunj511@newschool.edu>
2020-09-01 15:04:30 -0400

    Unity First Project

---
+++ FirstProject/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate(){ //When physics is happening
+        if (jump) {
+            jump = false;
+            rb.AddForce(Vector2.up * 5, ForceMode2D.Impulse); //Add an up force
+        }
+    }
+}
