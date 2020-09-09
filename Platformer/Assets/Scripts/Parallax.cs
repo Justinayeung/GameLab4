1c34f081c8154edb1d0aa9a0fa5f33b6bc726c45
Justinayeung <yeunj511@newschool.edu>
2020-09-09 14:37:55 -0400

    BG for Platformer

---
+++ Platformer/Assets/Scripts/Parallax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public GameObject cam;
+    public float parallaxEffect;
+
+    float length;
+    float startpos;
+
+    // Start is called before the first frame update
+    void Start() {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x; //Size (x) of sprite
+    }
+
+    void FixedUpdate() {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect)); //How far we move relative to the cam
+        float dist = (cam.transform.position.x * parallaxEffect); //How far we move in world space
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z); //Move Camera
+
+        if (temp > startpos + length) {
+            startpos += length;
+        } else if (temp < startpos - length) {
+            startpos -= length;
+        }
+    }
+}
