860692d231d9b9586760653f7d346fc8ac6292e1
Justinayeung <yeunj511@newschool.edu>
2020-09-17 17:28:00 -0400

    Unity

--- Platformer/Assets/Scripts/Enemy.cs
+++ Platformer/Assets/Scripts/Enemy.cs
@@ -7,12 +7,15 @@ public class Enemy : MonoBehaviour
     [Header("Variables")]
     public int health;
     public float speed;
+    public float rayDistance;
     public float startDazedTime;
 
     [Header("References")]
-    public GameObject attackTaken;
+    public GameObject attackTakenParticles;
+    public Transform groundDetection;
 
     float dazedTime;
+    bool movingLeft = true;
 
     // Start is called before the first frame update
     void Start() {
@@ -21,6 +24,18 @@ public class Enemy : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, rayDistance);
+        if(groundInfo.collider == false) { //If ray hasn't collided with anything
+            if(movingLeft) {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft = false;
+            } else {
+                transform.eulerAngles = new Vector3(0, -0, 0);
+                movingLeft = true;
+            }
+        }
+
         if(dazedTime <= 0) {
             speed = 5;
         } else {
@@ -36,7 +51,7 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage) {
         //Play hurt sounds
         dazedTime = startDazedTime;
-        Instantiate(attackTaken, transform.position, Quaternion.identity);
+        Instantiate(attackTakenParticles, transform.position, Quaternion.identity);
         health -= damage;
         Debug.Log("damage taken");
     }
