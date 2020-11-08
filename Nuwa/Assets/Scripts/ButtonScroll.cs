78d27a3fc7d77fc2244a222f4147e172bbe880e4
Justinayeung <42782126+Justinayeung@users.noreply.github.com>
2020-11-08 17:57:29 -0500

    Nuwa_Cursor

--- Nuwa/Assets/Scripts/ButtonScroll.cs
+++ Nuwa/Assets/Scripts/ButtonScroll.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class ButtonScroll : MonoBehaviour
 {
-    public int typeOfStone;
-
-    [Header("References")]
+    [Header("Stone Sprites")]
     public Sprite YellowStone;
     public Sprite RedStone;
     public Sprite BlueStone;
     public Sprite BlackStone;
     public Sprite WhiteStone;
 
+    [Header("Cursor Sprites")]
+    public Texture2D OriginalCursor;
+    public Texture2D YellowCursor;
+    public Texture2D RedCursor;
+    public Texture2D BlueCursor;
+    public Texture2D BlackCursor;
+    public Texture2D WhiteCursor;
+
+    [Header("Variables")]
+    public int typeOfStone;
+    public bool yellowStone;
+    public bool redStone;
+    public bool blueStone;
+    public bool blackStone;
+    public bool whiteStone;
+
     SpriteRenderer sr;
     Animator anim;
     bool canSwitch;
 
     void Start() {
+        Cursor.SetCursor(OriginalCursor, Vector2.zero, CursorMode.ForceSoftware);
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         typeOfStone = 0;
@@ -32,6 +47,7 @@ public class ButtonScroll : MonoBehaviour
             }
         }
         StoneSwitch();
+        checkingCursor();
     }
 
     void StoneSwitch() {
@@ -39,26 +55,31 @@ public class ButtonScroll : MonoBehaviour
             case 4: //Yellow Stone
                 if(canSwitch) { 
                     sr.sprite = YellowStone;
+                    yellowStone = true; redStone = false; blueStone = false; blackStone = false; whiteStone = false;
                 }
                 break;
             case 3: //Red Stone
                 if(canSwitch) {
                     sr.sprite = RedStone;
+                    yellowStone = false; redStone = true; blueStone = false; blackStone = false; whiteStone = false;
                 }
                 break;
             case 2: //Blue Stone
                 if(canSwitch) {
                     sr.sprite = BlueStone;
+                    yellowStone = false; redStone = false; blueStone = true; blackStone = false; whiteStone = false;
                 }
                 break;
             case 1: //Black Stone
                 if(canSwitch) {
-                    sr.sprite = BlackStone; 
+                    sr.sprite = BlackStone;
+                    yellowStone = false; redStone = false; blueStone = false; blackStone = true; whiteStone = false;
                 }
                 break;
             default: //White Stone
                 if(canSwitch) {
                     sr.sprite = WhiteStone;
+                    yellowStone = false; redStone = false; blueStone = false; blackStone = false; whiteStone = true;
                 }
                 break;
         }
@@ -73,5 +94,19 @@ public class ButtonScroll : MonoBehaviour
 
     public void CallSwitch() {
         canSwitch = true;
+    }
+
+    public void checkingCursor() {
+        if (yellowStone) {
+            Cursor.SetCursor(YellowCursor, Vector2.zero, CursorMode.ForceSoftware);
+        } else if(redStone) {
+            Cursor.SetCursor(RedCursor, Vector2.zero, CursorMode.ForceSoftware);
+        } else if(blueStone) {
+            Cursor.SetCursor(BlueCursor, Vector2.zero, CursorMode.ForceSoftware);
+        } else if(blackStone) {
+            Cursor.SetCursor(BlackCursor, Vector2.zero, CursorMode.ForceSoftware);
+        } else if(whiteStone) {
+            Cursor.SetCursor(WhiteCursor, Vector2.zero, CursorMode.ForceSoftware);
+        }
     }
 }
