e0c0581bcfc890d7a414693e328115608104e061
Justinayeung <42782126+Justinayeung@users.noreply.github.com>
2020-11-09 13:36:02 -0500

    Test2

--- Nuwa/Assets/Scripts/ButtonScroll.cs
+++ Nuwa/Assets/Scripts/ButtonScroll.cs
@@ -47,7 +47,6 @@ public class ButtonScroll : MonoBehaviour
             }
         }
         StoneSwitch();
-        checkingCursor();
     }
 
     void StoneSwitch() {
---
+++ Nuwa/Assets/Scripts/CheckMouseOver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMouseOver : MonoBehaviour
+{
+    ButtonScroll buttonScript;
+
+    private void Start() {
+        buttonScript = FindObjectOfType<ButtonScroll>();    
+    }
+
+    public void OnMouseOver() {
+        buttonScript.checkingCursor();
+    }
+
+    public void OnMouseExit() {
+        Cursor.SetCursor(buttonScript.OriginalCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
