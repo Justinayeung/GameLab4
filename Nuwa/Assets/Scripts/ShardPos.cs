37914ff331535733beca4c3f27e63df6e3347dec
Justinayeung <42782126+Justinayeung@users.noreply.github.com>
2020-11-10 19:31:02 -0500

    Unity_ShardTest

--- Nuwa/Assets/Scripts/ShardPos.cs
+++ Nuwa/Assets/Scripts/ShardPos.cs
@@ -4,22 +4,95 @@ using UnityEngine;
 
 public class ShardPos : MonoBehaviour
 {
+    [Header("To Broken Image")]
+    public float durationOfPos;
+    public float durationOfScale;
+    public Vector3 endPos;
+    public Vector3 endScale;
+    public float speed;
+    public bool intro;
+
+    [Header("Full Image")]
     public Vector3 originalPos;
-    public Animator movePos;
+    public float moveSpeedToOrgPos;
+
+    [Header("Broken Image")]
+    public Vector3 moveDirection;
+    public Vector3 startPos;
+    public float moveDistance;
+    public float moveSpeed;
+    public bool clicked;
+
+    [Header("Scale Componenets")]
+    public Vector3 orgScale; 
+    public float scaleToOrgTime;
+
+    Vector3 currentPos;
+
+    void Start() {
+        intro = true;
+        transform.position = originalPos;
+        transform.localScale = orgScale;
+        StartCoroutine(ShardBrokenPos(originalPos, endPos, durationOfPos));
+        StartCoroutine(ShardBrokenScale(orgScale, endScale, durationOfScale));
+    }
 
     void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        if(transform.position == endPos && transform.localScale == endScale) {
+            intro = false;
+        }
 
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if(hit.collider != null) {
-                Debug.Log("Hit");
+        currentPos = transform.position;
+
+        if(!clicked) {
+            if (!intro) { 
+                transform.position = startPos + moveDirection * (moveDistance*Mathf.Sin(Time.time*moveSpeed));
             }
+        } else {
+            Vector3 startingPos = currentPos;
+            Vector3 startingScale = transform.localScale;
+            StartCoroutine(ScaleOverTime(startingScale, orgScale, scaleToOrgTime));
+            StartCoroutine(PosOverTime(startingPos, originalPos, moveSpeedToOrgPos));
         }
     }
 
-    public void MovePosition() {
-        transform.position = originalPos;
+    public IEnumerator ScaleOverTime(Vector3 a, Vector3 b, float seconds) {
+        float i = 0.0f;
+        float rate = (1.0f/seconds) * seconds;
+        while(i < 1.0f) {
+            i += Time.deltaTime * rate;
+            transform.localScale = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+    }
+
+    public IEnumerator PosOverTime(Vector3 a, Vector3 b, float seconds) {
+        float i = 0.0f;
+        float rate = (1.0f/seconds) * seconds;
+        while(i < 1.0f) {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+    }
+
+    public IEnumerator ShardBrokenPos(Vector3 a, Vector3 b, float seconds) {
+        float i = 0.0f;
+        float rate = (1.0f/seconds) * speed;
+        while(i < 1.0f) {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+    }
+
+    public IEnumerator ShardBrokenScale(Vector3 a, Vector3 b, float seconds) {
+        float i = 0.0f;
+        float rate = (1.0f/seconds) * speed;
+        while(i < 1.0f) {
+            i += Time.deltaTime * rate;
+            transform.localScale = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
     }
 }
