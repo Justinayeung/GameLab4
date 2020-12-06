2ece9cfff0b28f5874df55fe4705211c8ef11d93
Justinayeung <42782126+Justinayeung@users.noreply.github.com>
2020-12-06 13:36:36 -0500

    Nuwa Mechanic Updates

---
+++ Nuwa/Assets/Scripts/DrawManager.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using PDollarGestureRecognizer;
+
+public class DrawManager : MonoBehaviour {
+    private Vector3 loc;
+    private GameManager gameManager;
+    public LayerMask layerMask;
+
+	public Transform gestureOnScreenPrefab;
+    public Transform spherePrefab;
+
+	private List<Gesture> trainingSet = new List<Gesture>();
+
+	private List<Point> points = new List<Point>();
+	private int strokeId = -1;
+
+	private Vector3 virtualKeyPosition = Vector2.zero;
+	private Rect drawArea;
+
+	private RuntimePlatform platform;
+	private int vertexCount = 0;
+
+	private List<LineRenderer> gestureLinesRenderer = new List<LineRenderer>();
+	private LineRenderer currentGestureLineRenderer;
+
+	//GUI
+	private string message;
+	private bool recognized;
+	private string newGestureName = "";
+
+	void Start () {
+        gameManager = FindObjectOfType<GameManager>();
+		platform = Application.platform;
+		drawArea = new Rect(0, 0, Screen.width, Screen.height);
+
+		//Load pre-made gestures
+		TextAsset[] gesturesXml = Resources.LoadAll<TextAsset>("GestureSet/10-stylus-MEDIUM/");
+		foreach (TextAsset gestureXml in gesturesXml)
+			trainingSet.Add(GestureIO.ReadGestureFromXML(gestureXml.text));
+
+		//Load user custom gestures
+		string[] filePaths = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+		foreach (string filePath in filePaths)
+			trainingSet.Add(GestureIO.ReadGestureFromFile(filePath));
+	}
+
+	void Update () {
+        if(gameManager.isDrawing) { 
+		    if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer) {
+			    if (Input.touchCount > 0) {
+				    virtualKeyPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y);
+			    }
+		    } else {
+			    if (Input.GetMouseButton(0)) {
+				    virtualKeyPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+			    }
+		    }
+
+		    if (drawArea.Contains(virtualKeyPosition)) {
+
+			    if (Input.GetMouseButtonDown(0)) {
+
+				    if (recognized) {
+
+					    recognized = false;
+					    strokeId = -1;
+
+					    points.Clear();
+
+					    foreach (LineRenderer lineRenderer in gestureLinesRenderer) {
+
+						    lineRenderer.SetVertexCount(0);
+						    Destroy(lineRenderer.gameObject);
+					    }
+
+					    gestureLinesRenderer.Clear();
+				    }
+
+				    ++strokeId;
+				
+				    Transform tmpGesture = Instantiate(gestureOnScreenPrefab, transform.position, transform.rotation) as Transform;
+				    currentGestureLineRenderer = tmpGesture.GetComponent<LineRenderer>();
+				
+				    gestureLinesRenderer.Add(currentGestureLineRenderer);
+				
+				    vertexCount = 0;
+			    }
+			
+			    if (Input.GetMouseButton(0)) {
+				    points.Add(new Point(virtualKeyPosition.x, -virtualKeyPosition.y, strokeId));
+
+				    currentGestureLineRenderer.SetVertexCount(++vertexCount);
+				    currentGestureLineRenderer.SetPosition(vertexCount - 1, Camera.main.ScreenToWorldPoint(new Vector3(virtualKeyPosition.x, virtualKeyPosition.y, 10)));
+			    }
+		    }
+        }
+	}
+
+    public void TryRecognize() {
+        if(points.Count <= 0) {
+            return;
+        }
+
+        if(recognized) {
+            ClearLine();
+        }
+
+        recognized = true;
+        Gesture candidate = new Gesture(points.ToArray());
+        Result gestureResult = PointCloudRecognizer.Classify(candidate, trainingSet.ToArray());
+
+        if(gestureResult.Score < 0.75f) {
+            ClearLine();
+            return;
+        }
+
+        if(gestureResult.GestureClass == "Circle") {
+            Transform b = Instantiate(spherePrefab, gestureLinesRenderer[0].bounds.center, Quaternion.identity);
+
+            if (recognized) {
+                recognized = false;
+                strokeId = -1;
+                points.Clear();
+                foreach(LineRenderer lineRenderer in gestureLinesRenderer) {
+                    lineRenderer.SetVertexCount(0);
+                    Destroy(lineRenderer.gameObject);
+                }
+                gestureLinesRenderer.Clear();
+            }
+        }
+    }
+
+    public void ClearLine() {
+        recognized = false;
+        strokeId = -1;
+        points.Clear();
+        foreach(LineRenderer lineRenderer in gestureLinesRenderer) {
+            lineRenderer.positionCount = 0;
+            Destroy(lineRenderer.gameObject);
+        }
+        gestureLinesRenderer.Clear();
+    }
+}
---
+++ Nuwa/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Camera brushCamera;
+
+    public Cinemachine.CinemachineVirtualCamera virtualCam;
+    public Renderer drawingRenderer;
+    public bool isDrawing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            virtualCam.enabled = false;
+            setDrawing(!isDrawing);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            virtualCam.enabled = true;
+            setDrawing(!isDrawing);
+        }
+    }
+
+    public void setDrawing(bool state) {
+        if (state == true) {
+            Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer("Default"));
+            Camera.main.cullingMask &= ~(1 << LayerMask.NameToLayer("Interactables"));
+        } else {
+            Camera.main.cullingMask |= 1 << LayerMask.NameToLayer("Default");
+            Camera.main.cullingMask |= 1 << LayerMask.NameToLayer("Interactables");
+        }
+
+        isDrawing = state;
+        virtualCam.enabled = !state;
+        drawingRenderer.enabled = state;
+       // drawingRenderer.transform.GetChild(0).gameObject.SetActive(state);
+        if(state == false) {
+            FindObjectOfType<DrawManager>().TryRecognize();
+        }
+    }
+}
